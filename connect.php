49835c557d46d88cce993cf16c40098ec1828f02
Joesph Parker <zbf6@fresnou.org>
2018-11-02 20:34:45 +0000

    Caught up

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "zeromile";
+$password = "";
+$dbname = "c9";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error);
+}
+//echo "Connected successfully, yo";
+?>
